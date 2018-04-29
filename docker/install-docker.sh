d5f027e1da5ece8a1415ba29efbb555fd8e0972f
Ryan Northey <ryan@synca.io>
2018-04-29 23:50:01 +0100

    Add docker recipe

---
+++ docker/install-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+apt-get update
+apt-get install -qq \
+     --no-install-recommends \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -qq --no-install-recommends docker-ce
