5450072bef136add9f891aadaa424f3cf2cdcb06
Ryan Northey <ryan@synca.io>
2018-05-04 09:45:19 +0100

    Add criu build in controller recipe

---
+++ docker/install-build.sh
@@ -0,0 +1,4 @@
+
+
+apt-get update -qq
+apt-get install -qq --no-install-recommends bzip2 libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev libnet1-dev build-essential
