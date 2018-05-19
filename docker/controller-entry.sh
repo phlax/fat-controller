482652a2a512b88475c4b68781b997fe255c9605
Ryan Northey <ryan@synca.io>
2018-05-19 21:19:50 +0100

    Setup zmq pipes

--- docker/controller-entry.sh
+++ docker/controller-entry.sh
@@ -2,5 +2,6 @@
 
 mkdir -p /var/lib/controller
 echo "USE_CRIU=$USE_CRIU" > /var/lib/controller/env
-
+echo "LISTEN_ZMQ=$LISTEN_ZMQ" >> /var/lib/controller/env
+echo "PUBLISH_ZMQ=$PUBLISH_ZMQ" >> /var/lib/controller/env
 exec /lib/systemd/systemd $@
