55c25018608f365549e4174732de28100625d262
Ryan Northey <ryan@synca.io>
2018-05-27 19:16:27 +0100

    Update fatc recipe
    - new daemon system
    - remove all python
    - handle frontend/backend unix/tcp properly
    - improved
      - idle handling
      - service start/stop
    - updated criu

--- docker/bin/controller-entry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p /var/lib/controller
-echo "USE_CRIU=$USE_CRIU" > /var/lib/controller/env
-echo "LISTEN_ZMQ=$LISTEN_ZMQ" >> /var/lib/controller/env
-echo "PUBLISH_ZMQ=$PUBLISH_ZMQ" >> /var/lib/controller/env
-exec /lib/systemd/systemd $@
