61ba298e4a85753b7766206c4d6f575c3148400d
Ryan Northey <ryan@synca.io>
2020-05-09 05:36:58 +0100

    Fix paths in tests and example

--- tests/daemons.sh
+++ tests/daemons.sh
@@ -10,39 +10,39 @@ docker-compose up -d
 ### Daemon 0
 
 ## unix socket for daemon0 is setup and listening
-$D0_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$D0_STATUS--proxy -p ActiveState | grep inactive
+$D0_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$D0_STATUS.0--proxy -p ActiveState | grep inactive
 $D0_STATUS -p ActiveState | grep -v inactive | grep active
 
 ## unix socket for daemon0 is responding to http requests
 $CURL --unix-socket ./example/sockets/fc/daemon0.0.sock http://localhost | grep Hello | grep backend | grep unix
-$D0_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$D0_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 $D0_STATUS | grep GET
 $D0_COMPOSE logs http | grep GET
 
 ### Daemon 1
 
 # unix socket for daemon1 is setup and listening,
-$D1_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$D1_STATUS--proxy -p ActiveState | grep inactive
+$D1_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$D1_STATUS.0--proxy -p ActiveState | grep inactive
 $D1_STATUS | grep HEAD
 
 # unix socket for daemon1 is responding to http requests
 $CURL --unix-socket ./example/sockets/fc/daemon1.0.sock http://localhost | grep Hello | grep backend | grep network
-$D1_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$D1_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 $D1_STATUS | grep GET
 $D1_COMPOSE logs http | grep GET
 
 ### Daemon 2
 
 ## http socket for daemon2 is setup and listening
-$D2_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$D2_STATUS--proxy -p ActiveState | grep inactive
+$D2_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$D2_STATUS.0--proxy -p ActiveState | grep inactive
 $D2_STATUS | grep HEAD
 
 ## http socket for daemon2 is responding to http requests
 $CURL http://localhost:8082 | grep Hello | grep backend | grep unix
-$D2_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$D2_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 $D2_STATUS | grep GET
 $D2_COMPOSE logs http | grep GET
 
@@ -50,13 +50,13 @@ $D2_COMPOSE logs http | grep GET
 ### Daemon 3
 
 ## http socket for daemon3 is setup and listening
-$D3_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$D3_STATUS--proxy -p ActiveState | grep inactive
+$D3_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$D3_STATUS.0--proxy -p ActiveState | grep inactive
 $D3_STATUS | grep HEAD
 
 ## http socket for daemon3 is responding to http requests
 $CURL http://localhost:8083 | grep Hello | grep backend | grep network
-$D3_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$D3_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 $D3_STATUS | grep GET
 $D3_COMPOSE logs http_daemon3 | grep GET
 
--- tests/services.sh
+++ tests/services.sh
@@ -11,13 +11,13 @@ docker-compose up -d
 ### SERVICE 0
 
 ## unix socket for service0 is setup and listening
-$S0_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$S0_STATUS--proxy -p ActiveState | grep inactive
+$S0_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$S0_STATUS.0--proxy -p ActiveState | grep inactive
 $S0_STATUS -p ActiveState | grep inactive
 
 ## unix socket for service0 is responding to http requests
 $CURL --unix-socket ./example/sockets/fc/service0.0.sock http://localhost | grep Hello | grep backend | grep unix
-$S0_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$S0_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 $S0_STATUS -p ActiveState | grep -v inactive | grep active
 # these tests fail as it doesnt write to the log quickly enough
 # $S0_STATUS -n500 | grep HEAD
@@ -27,13 +27,13 @@ $S0_COMPOSE logs http | grep GET
 ### SERVICE 1
 
 ## unix socket for service1 is setup and listening
-$S1_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$S1_STATUS--proxy -p ActiveState | grep inactive
+$S1_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$S1_STATUS.0--proxy -p ActiveState | grep inactive
 $S1_STATUS -p ActiveState | grep inactive
 
 ## unix socket for service1 is responding to http requests
 $CURL --unix-socket ./example/sockets/fc/service1.0.sock http://localhost | grep Hello | grep backend | grep network
-$S1_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$S1_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 $S1_STATUS -p ActiveState | grep -v inactive | grep active
 # $S1_STATUS | grep HEAD
 $S1_STATUS | grep GET
@@ -42,13 +42,13 @@ $S1_COMPOSE logs http_service1 | grep GET
 ### SERVICE 2
 
 ## http socket for service2 is setup and listening
-$S2_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$S2_STATUS--proxy -p ActiveState | grep inactive
+$S2_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$S2_STATUS.0--proxy -p ActiveState | grep inactive
 $S2_STATUS -p ActiveState | grep inactive
 
 ## http socket for service2 is responding to http requests
 $CURL http://localhost:8092 | grep Hello | grep backend | grep unix
-$S2_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$S2_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 $S2_STATUS -p ActiveState | grep -v inactive | grep active
 # $S2_STATUS | grep HEAD
 # $S2_STATUS | grep GET
@@ -57,13 +57,13 @@ $S2_COMPOSE logs http | grep GET
 ### SERVICE 3
 
 ## http socket for service3 is setup and listening
-$S3_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$S3_STATUS--proxy -p ActiveState | grep inactive
+$S3_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$S3_STATUS.0--proxy -p ActiveState | grep inactive
 $S3_STATUS -p ActiveState | grep inactive
 
 ## http socket for service3 is responding to http requests
 $CURL http://localhost:8093 | grep Hello | grep backend | grep network
-$S3_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$S3_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 $S3_STATUS -p ActiveState | grep -v inactive | grep active
 # $S3_STATUS | grep HEAD
 # $S3_STATUS | grep GET
@@ -75,23 +75,23 @@ $S3_COMPOSE logs http_service3 | grep GET
 ### Backend/external services (Service4/5)
 
 ## http socket for service4 is setup and listening
-$S4_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$S4_STATUS--proxy -p ActiveState | grep inactive
+$S4_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$S4_STATUS.0--proxy -p ActiveState | grep inactive
 $S4_STATUS -p ActiveState | grep inactive
 
 ## http socket for service4 is responding to http requests
 $CURL --unix-socket ./example/sockets/fc/service4.0.sock http://localhost | grep Hello | grep external | grep backend
-$S4_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$S4_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 # $S4_STATUS -p ActiveState | grep -v inactive | grep active
 
 ## http socket for service5 is setup and listening
-$S5_STATUS--proxy.socket -p ActiveState | grep -v inactive | grep active
-$S5_STATUS--proxy -p ActiveState | grep inactive
+$S5_STATUS.0--proxy.socket -p ActiveState | grep -v inactive | grep active
+$S5_STATUS.0--proxy -p ActiveState | grep inactive
 $S5_STATUS -p ActiveState | grep inactive
 
 ## http socket for service5 is responding to http requests
 $CURL http://localhost:8095 | grep Hello | grep external | grep backend
-$S5_STATUS--proxy -p ActiveState | grep -v inactive | grep active
+$S5_STATUS.0--proxy -p ActiveState | grep -v inactive | grep active
 # $S5_STATUS -p ActiveState | grep -v inactive | grep active
 
 docker-compose down
