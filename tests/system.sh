e43dca6241d76d9d3d0812ff620730888d89604c
Ryan Northey <ryan@synca.io>
2020-06-04 15:47:50 +0100

    Add templates test

--- tests/system.sh
+++ tests/system.sh
@@ -77,4 +77,11 @@ $FC_EXEC su -s /bin/sh fatc -c "sudo ip route | grep docker0"
 $FC_EXEC su -s /bin/sh fatc -c "sudo iptables -L | grep DOCKER-USER"
 
 
+## templates
+
+stackhash=$($FC_EXEC hashtree -i ".tpls/*" -c .tpls/ignore hash /var/lib/fatc/templates-registry/stack)
+$FC_EXEC fatctl templates pull
+$FC_EXEC fatctl templates list | grep "$stackhash"
+
+
 docker-compose down
